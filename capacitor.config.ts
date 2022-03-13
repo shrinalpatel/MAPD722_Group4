b05f1134e1e68418d4c28b9e2a4404a895dae73d
Shrinal Patel <shrinalpatel98@gmail.com>
2022-03-12 22:23:14 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MAPD722_M2_Group4',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
